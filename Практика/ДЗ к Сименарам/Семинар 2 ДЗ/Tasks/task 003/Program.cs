9298c71e269cee9208e42cf549761f0c71a9baa6
Artem Zhurba <art201200@yandex.ru>
2023-10-11 18:49:44 +0300

    добавил задачу номер 2

--- Практика/ДЗ к Сименарам/Семинар 2 ДЗ/Tasks/task 003/Program.cs
+++ Практика/ДЗ к Сименарам/Семинар 2 ДЗ/Tasks/task 003/Program.cs
@@ -1,5 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Net.WebSockets;
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//326791123 -> 6
 
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("привет");
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+
+System.Console.WriteLine("Введите данные");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+while (num >999 )
+{
+     num /= 10;
+      
+}
+
+    if(num < 100)
+    {
+    System.Console.WriteLine("Третьего числа нет");
+    }
+
+else 
+{
+     System.Console.WriteLine("" + num % 10);
+}     
