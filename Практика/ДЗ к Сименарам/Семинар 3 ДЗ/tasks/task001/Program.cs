2f285c86f9abd5a09e61b8e1aa164cfeba2a43c5
Artem Zhurba <art201200@yandex.ru>
2023-10-18 14:43:01 +0300

    добавил задача  25

--- Практика/ДЗ к Сименарам/Семинар 3 ДЗ/tasks/task001/Program.cs
+++ Практика/ДЗ к Сименарам/Семинар 3 ДЗ/tasks/task001/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+// void NatSteps(int a  ) // (int a , int b) 
+// {
+//     double result = Math.Pow(a, 5);  // (a . b)
+//     System.Console.WriteLine(result);
+// }
+
+// NatStep(numA, numB);
+
+void NatStep(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    System.Console.WriteLine(result);
+
+ }
+
+ System.Console.WriteLine("Задай A");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Задай B");
+int numB = Convert.ToInt32(Console.ReadLine());
  
+ 
+//  NatSteps(numA); // (numA , numB)
+
+ NatStep(numA, numB);
+
+
