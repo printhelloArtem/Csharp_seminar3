8791733c0a97708b620ee3c639b99f998c87a933
Artem Zhurba <art201200@yandex.ru>
2023-10-18 14:58:30 +0300

    добавил задача  25

--- Практика/ДЗ к Сименарам/Семинар 3 ДЗ/tasks/task_025/Program.cs
+++ Практика/ДЗ к Сименарам/Семинар 3 ДЗ/tasks/task_025/Program.cs
@@ -1,18 +1,15 @@
 ﻿
-
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
 
-
-// void NatSteps(int a  ) // (int a , int b) 
+// void NatStep(int a, int b)
 // {
-//     double result = Math.Pow(a, 5);  // (a . b)
+//     double result = Math.Pow(a, b);
 //     System.Console.WriteLine(result);
 // }
 
-// NatStep(numA, numB);
 
 void NatStep(int a, int b)
 {
@@ -22,17 +19,12 @@ void NatStep(int a, int b)
         result = result * a;
     }
     System.Console.WriteLine(result);
+}
 
- }
 
- System.Console.WriteLine("Задай A");
+System.Console.WriteLine("Задай A");
 int numA = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Задай B");
 int numB = Convert.ToInt32(Console.ReadLine());
- 
- 
-//  NatSteps(numA); // (numA , numB)
 
- NatStep(numA, numB);
-
-
+NatStep(numA, numB);
---
+++ Практика/Сименары/Семинар 4/задачи/Program.cs
@@ -0,0 +1,174 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// _тип_данных_     _название_функции_ (_принимаемые_параметры_)
+// {
+//     _тело_функции_
+// }
+
+
+// int SumNumbers(int A)
+// {
+//     int sum = 0;
+
+//     for(int i = 1; i <= A; i++)
+//     {
+//         sum += i;
+//     }
+
+//     return sum;
+// }
+
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Math.Abs(Convert.ToInt32(Console.ReadLine())); 
+// }
+
+// // -----------------------------------------------
+
+// int number = ReadInt("Введите число: ");
+// System.Console.WriteLine(SumNumbers(number));
+
+
+// Задача 26: Напишите программу, которая принимает на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// int ReadIntAbs(string text)
+// {
+//     System.Console.Write(text);
+//     return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// }
+
+// int LenNumber(int value)
+// {
+//     int len = 0;
+
+//     while (value > 0)
+//     {
+//         value /= 10;
+//         len++;
+//     }
+
+//     return len;
+// }
+
+// // -------------------------------
+
+// int number = ReadIntAbs("Введите число: ");
+
+// Console.WriteLine(LenNumber(number));
+
+
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+// using System.Runtime.Intrinsics.Arm;
+
+// int ReadIntAbs(string text)
+// {
+//     System.Console.Write(text);
+//     return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// }
+
+// int MultNumbers(int N)
+// {
+//     int result = 1;
+
+//     for (int i = 2; i <= N; i++)
+//     {
+//         result *= i;
+//     }
+
+//     return result;
+// }
+// // -------------------------------
+
+// int number = ReadIntAbs("Введите число: ");
+// System.Console.WriteLine(MultNumbers(number));
+
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// void FillArray(int[] arrayToFill)
+// {
+//     Random rand = new Random();
+
+//     for (int i = 0; i < arrayToFill.Length; i++)
+//     {
+//         arrayToFill[i] = rand.Next(2);
+//     }
+// }
+
+// void PrintArray(int[] arrayForPrint)
+// {
+//     for (int i = 0; i < arrayForPrint.Length; i++)
+//     {
+//         System.Console.Write(arrayForPrint[i] + " ");
+//     }
+// }
+
+// // -----------------------------
+
+// int[] array = new int[8];
+// FillArray(array);
+// PrintArray(array);
+
+
+
+using System.Xml.XPath;
+
+void PrinWrite(string text1, string text2)
+{
+    System.Console.WriteLine(text1);
+    Math.Abs(Convert.ToInt32(System.Console.ReadLine()));
+
+     System.Console.WriteLine(text2);
+
+
+    Math.Abs(Convert.ToInt32(System.Console.ReadLine()));
+}
+
+
+
+
+// int Sum(int a ,  int b)
+// {
+//     int resul = a + b;
+
+// return resul;
+// }
+
+
+
+
+
+//  PrinWrite( "Введите число 1" ,"Введите число 2");
+
+
+
+
+
+// System.Console.WriteLine("Введите число");
+// int v = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(System.Console.ReadLine());
+
+PrinWrite("Введитее число" ,"Введите число 2" );
+//  System.Console.WriteLine(Sum(  ));
+int d = a;
+int n =b;
+int resul = d + n;
+
+System.Console.WriteLine( "" + resul);
