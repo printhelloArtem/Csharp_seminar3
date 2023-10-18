3cb2024fdce27915d80a0a5561f6f598d5730156
Artem Zhurba <art201200@yandex.ru>
2023-10-18 15:25:16 +0300

    добавил задача  27

---
+++ Практика/ДЗ к Сименарам/Семинар 3 ДЗ/tasks/task_027/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int SumNumber(int a)
+{
+
+    int resault = 0;
+    for (int i = a; i > 0; i/=10)
+
+    {  
+        resault  = resault + i % 10;
+    }
+    return resault;
+}
+
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+int userRes = (SumNumber(num));
+
+System.Console.WriteLine(userRes);
