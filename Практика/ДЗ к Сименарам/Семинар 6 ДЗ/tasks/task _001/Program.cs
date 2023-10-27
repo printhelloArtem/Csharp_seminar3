6aeb172a56b6ecbd6352cbc4015d4b18f793bb16
Artem Zhurba <art201200@yandex.ru>
2023-10-27 17:58:54 +0300

    задачи с семинара 6 дз

---
+++ Практика/ДЗ к Сименарам/Семинар 6 ДЗ/tasks/task _001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+System.Console.Write("Укажите количество чисел: ");
+int value = int.Parse(System.Console.ReadLine());
+
+int count = 0;
+
+for(int i = 0; i < value; i++)
+{
+System.Console.Write("Введите число: ");
+if(int.Parse(System.Console.ReadLine()) > 0 ) count++;
+}
+
+System.Console.WriteLine("Чисел со значением больше нуля было введено: " + count);
