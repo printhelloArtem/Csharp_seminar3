b6b4d5d2ae5efed4c9474bf05cb6d9ba0782cf02
Artem Zhurba <art201200@yandex.ru>
2023-11-13 22:04:55 +0300

    задачи с семинара 8

---
+++ Практика/Сименары/Семинар 8/Tasks/task_4/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+// на пересечении которых расположен наименьший элемент массива.
+
+int[,] GenerateMatrix(int row, int col)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[row, col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rand.Next(10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindMinNumberIndexes(int[,] myMatrix, out int minRow, out int minCol)
+{
+    minRow = 0;
+    minCol = 0;
+
+    for (int i = 0; i < myMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < myMatrix.GetLength(1); j++)
+        {
+            if (myMatrix[minRow, minCol] > myMatrix[i, j])
+            {
+                minRow = i;
+                minCol = j;
+            }
+        }
+    }
+}
+
+int[,] DeleteRowAndColInMatrix(int[,] myMatrix, int minRow, int minCol)
+{
+    int[,] newMatrix = new int[myMatrix.GetLength(0) - 1, myMatrix.GetLength(1) - 1];
+
+    for (int i = 0, x = 0; i < myMatrix.GetLength(0); i++)
+    {
+        if (i == minRow) continue;
+        for (int j = 0, y = 0; j < myMatrix.GetLength(1); j++)
+        {
+            if (j == minCol) continue;
+            newMatrix[x, y] = myMatrix[i, j];
+            y++;
+        }
+        x++;
+    }
+
+    return newMatrix;
+}
+
+// --------------------------------------------
+
+int[,] myMatrix = GenerateMatrix(5, 5);
+PrintMatrix(myMatrix);
+
+FindMinNumberIndexes(myMatrix, out int minRow, out int minCol);
+
+int[,] newMatrix = DeleteRowAndColInMatrix(myMatrix, minRow, minCol);
+
+System.Console.WriteLine();
+PrintMatrix(newMatrix);
